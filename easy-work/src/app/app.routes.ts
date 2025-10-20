dc41a0993a03a7c26b337f33849b5dcb4810f7db
Lilichka tyanochka <lilu@pss1.ukr.education>
2025-10-20 09:02:27 +0300

    add test component

--- easy-work/src/app/app.routes.ts
+++ easy-work/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main-component/main-component';
+import { TestComponent } from './test-component/test-component';
 
 export const routes: Routes = [
-    {path:"main", component : MainComponent}
+    {path:"main", component : MainComponent},
+    {path:"test", component : TestComponent}
 ];
---
+++ easy-work/src/app/test-component/test-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  imports: [],
+  templateUrl: './test-component.html',
+  styleUrl: './test-component.css'
+})
+export class TestComponent {
+
+}
